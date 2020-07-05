fd0f555fb5b1166e061bc5d942558520b7779ef1
wuchangsheng951 <sunhuiming55@gmail.com>
2020-07-04 20:33:05 -0400

    'version1.0.0'

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+    name='gpugo',
+    version='0.1.0',
+    packages=['gpugo'],
+    description='Show GPU information and deep learning tasks assignment',
+    author='Huiming Sun',
+    author_email='sunhuiming55@gmail.com',
+    install_requires=[
+        'click',
+        'pandas',
+        'loguru',
+    ],
+
+    url='https://github.com/wuchangsheng951/gpugo',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    entry_points='''
+        [console_scripts]
+        gas=gpugo:main
+    ''',
+)
